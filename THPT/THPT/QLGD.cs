0c282dc8b672c0c74fce45e7abf982a9660c1b4e
cuong top <cuongtop4598>
2020-09-15 22:26:26 +0700

    hoàn thiện form

--- THPT/THPT/QLGD.cs
+++ THPT/THPT/QLGD.cs
@@ -18,27 +18,17 @@ namespace THPT
         {
             InitializeComponent();
         }
-        SqlConnection con;
+
         private void QLGD_Load(object sender, EventArgs e)
         {
-            con = new SqlConnection(@"Data Source=LAPTOP-KBK3516B\SQLEXPRESS;Initial Catalog=QuanLySinhVien;Integrated Security=True");
-            con.Open();
-            HienThi();
 
         }
-        private void HienThi()
-        {
-            string sqlSelect = "SELECT * FROM tblGIAOVIEN";
-            SqlCommand cmd = new SqlCommand(sqlSelect, con);
-            SqlDataReader dr = cmd.ExecuteReader();
-            DataTable dt = new DataTable();
-            dt.Load(dr);
-            dataGridView1.DataSource = dt;
-        }
+  
+       
 
         private void QLGD_FormClosing(object sender, FormClosingEventArgs e)
         {
-            con.Close();
+        
         }
 
         private void btnThem_Click(object sender, EventArgs e)
